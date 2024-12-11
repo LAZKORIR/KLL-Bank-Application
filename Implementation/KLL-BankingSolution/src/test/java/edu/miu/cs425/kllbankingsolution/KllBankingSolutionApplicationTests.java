a436b1bafb87341a8648d39fdf5ea56a91819693
LAZKORIR <lazaruskorir95@gmail.com>
2024-12-11 13:37:58 -0600

    initial project commit

---
+++ Implementation/KLL-BankingSolution/src/test/java/edu/miu/cs425/kllbankingsolution/KllBankingSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs425.kllbankingsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KllBankingSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
